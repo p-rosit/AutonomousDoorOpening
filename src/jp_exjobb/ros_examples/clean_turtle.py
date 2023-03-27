2bf407d9d1108d7a717819b321be503cddff11d5
chokladmousse <pontus.rosqvist@gmail.com>
2023-03-27 13:20:04 +0000

    :)

---
+++ src/jp_exjobb/ros_examples/clean_turtle.py
@@ -0,0 +1,11 @@
+import os
+import argparse
+
+parser = argparse.ArgumentParser(description='')
+parser.add_argument('mode', choices=['weak', 'agressive'], help='Mode :)', default="agressive")
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
